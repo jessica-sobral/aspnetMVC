65e5b0ed8fd17d9d6b0e743f85f3a5a099769f13
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-08-06 14:17:44 -0300

    add contacts table

--- Database/DatabaseSetup.cs
+++ Database/DatabaseSetup.cs
@@ -1,10 +1,32 @@
+using Microsoft.Data.Sqlite;
+
 namespace controlOfContacts.Database;
 
 public class DatabaseSetup
 {
     private readonly DatabaseConfig _databaseConfig;
+
     public DatabaseSetup(DatabaseConfig databaseConfig)
     {
         _databaseConfig = databaseConfig;
+    }
+
+    private void CreateContactTable()
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Contacts(
+                id int not null primary key,
+                name varchar(100) not null,
+                email varchar(100) not null,
+                telephone varchar(12) not null
+            );
+        ";
+
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 }
